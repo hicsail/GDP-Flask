9aa970be14b7544b39672961e9cedeef253e02c2
wenhwang97 <wenhwang@bu.edu>
2024-02-08 16:04:10 -0500

    chore: integrate translator

---
+++ translator/app.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+from dotenv import load_dotenv
+
+import os
+import translator
+import requests
+
+app = Flask(__name__)
+scheduler = BackgroundScheduler()
+
+@app.route("/health")
+def health_check():
+    return "healthy"
+
+def translate():
+    print("[MOF Translator] Translating started at " + datetime.now().isoformat() + "\n")
+    url = os.getenv("NOCO_DB_URL")
+    headers = {"xc-token": os.getenv("NOCO_XC_TOKEN")}
+    params = {
+        "where": "(isEnglish,eq,false)",
+        "limit": 10, # translate 10 records at a time
+    }
+    res = requests.get(url, headers=headers, params=params)
+    if res.json().get("pageInfo").get("totalRows") == 0:
+        print("[MOF Translator] No records to translate\n")
+        return
+    
+    translator_instance = translator.Translator()
+    target_field = {
+        "originalTitle": "translatedTitle",
+        "originalContent": "translatedContent",
+        "originalOutlet": "translatedOutlet"
+    }
+
+    for record in res.json().get("list"):
+        for key in record.keys():
+            if key in target_field.keys():
+                language = translator_instance.detect_lang_google(record.get(key))
+                if language != "en" and language != "und":
+                    record[target_field[key]] = translator_instance.translate_text_deepl(record.get(key)).text
+                    # record[target_field[key]] = translator_instance.translate_text_google(record.get(key))
+
+        record["isEnglish"] = True
+        requests.patch(url, headers=headers, json=record)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    scheduler.add_job(translate, "cron", hour="*", minute=0)
+    scheduler.start()
+    app.run(port=5002)
---
+++ translator/translator.py
@@ -0,0 +1,32 @@
+import deepl
+import requests
+import os
+
+class Translator:
+    def __init__(self):
+        self.deepl_key = os.getenv("DEEPL_API_KEY")
+        self.deepl_translator = deepl.Translator(self.deepl_key)
+
+        self.google_key = os.getenv("GOOGLE_API_KEY")
+        self.google_url = "https://translation.googleapis.com/language/translate/v2"
+
+    def translate_text_deepl(self, text, target_lang="EN-US"):
+        return self.deepl_translator.translate_text(text, target_lang=target_lang)
+    
+    def translate_text_google(self, text, target_lang="en"):
+        param = { "q": text, "target": target_lang, "key": self.google_key }
+        res = requests.post(self.google_url, params=param)
+
+        if res.status_code != 200:
+            return None
+        
+        return res.json()["data"]["translations"][0]["translatedText"]
+    
+    def detect_lang_google(self, text):
+        param = { "q": text, "key": self.google_key }
+        res = requests.post(self.google_url + "/detect", params=param)
+
+        if res.status_code != 200:
+            return None
+        
+        return res.json()["data"]["detections"][0][0]["language"]

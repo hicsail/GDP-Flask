e2d5c501536d353820378a7b8ca1d9aaa5e6c2a7
Greg Frasco <frascog17@gmail.com>
2024-02-15 13:27:48 -0500

    fix: add logs to translator, add local docker compose

--- translator/app.py
+++ translator/app.py
@@ -15,35 +15,40 @@ def health_check():
     return "healthy"
 
 def translate():
-    print("[MOF Translator] Translating started at " + datetime.now().isoformat() + "\n")
-    url = os.getenv("NOCO_DB_URL")
-    headers = {"xc-token": os.getenv("NOCO_XC_TOKEN")}
-    params = {
-        "where": "(isEnglish,eq,false)",
-        "limit": 10, # translate 10 records at a time
-    }
-    res = requests.get(url, headers=headers, params=params)
-    if res.json().get("pageInfo").get("totalRows") == 0:
-        print("[MOF Translator] No records to translate\n")
-        return
+    try:
 
-    translator_instance = translator.Translator()
-    target_field = {
-        "originalTitle": "translatedTitle",
-        "originalContent": "translatedContent",
-        "originalOutlet": "translatedOutlet"
-    }
+        print("[MOF Translator] Translating started at " + datetime.now().isoformat() + "\n")
+        url = os.getenv("NOCO_DB_URL")
+        headers = {"xc-token": os.getenv("NOCO_XC_TOKEN")}
+        params = {
+            "where": "(isEnglish,eq,false)",
+            "limit": 10, # translate 10 records at a time
+        }
+        res = requests.get(url, headers=headers, params=params)
+        if res.json().get("pageInfo").get("totalRows") == 0:
+            print("[MOF Translator] No records to translate\n")
+            return
 
-    for record in res.json().get("list"):
-        for key in record.keys():
-            if key in target_field.keys():
-                language = translator_instance.detect_lang_google(record.get(key))
-                if language != "en" and language != "und":
-                    # record[target_field[key]] = translator_instance.translate_text_deepl(record.get(key)).text
-                    record[target_field[key]] = translator_instance.translate_text_google(record.get(key))
+        translator_instance = translator.Translator()
+        target_field = {
+            "originalTitle": "translatedTitle",
+            "originalContent": "translatedContent",
+            "originalOutlet": "translatedOutlet"
+        }
 
-        record["isEnglish"] = True
-        requests.patch(url, headers=headers, json=record)
+        for record in res.json().get("list"):
+            for key in record.keys():
+                if key in target_field.keys():
+                    language = translator_instance.detect_lang_google(record.get(key))
+                    if language != "en" and language != "und":
+                        # record[target_field[key]] = translator_instance.translate_text_deepl(record.get(key)).text
+                        record[target_field[key]] = translator_instance.translate_text_google(record.get(key))
+
+            record["isEnglish"] = True
+            requests.patch(url, headers=headers, json=record)
+            print(f"[MOF Translator] Translated record: {record.get('originalTitle')}\n to {record.get('translatedTitle')}\n")
+    except Exception as e:
+        print(f"[MOF Translator] Error: {e}")
 
 
 if __name__ == "__main__":

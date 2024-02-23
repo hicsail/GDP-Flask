bbccaf0b32060cc2da84028eea5467accd0c20a0
wenhwang97 <wenhwang@bu.edu>
2024-02-23 16:25:54 -0500

    fix: fix datetime format

--- scraper/app.py
+++ scraper/app.py
@@ -165,7 +165,7 @@ def scrape():
             date_req = requests.get(url, headers=headers, params=date_params)
             if date_req.json().get("pageInfo").get("totalRows") > 0:
                 date_est = date_req.json().get("list")[0].get("articlePublishDateEst")
-                date_obj = datetime.strptime(date_est, "%Y-%m-%d %H:%M")
+                date_obj = datetime.fromisoformat(date_est)
                 est_time = est_tz.localize(date_obj)
                 beijing_time = est_time.astimezone(beijing_tz)
                 date = beijing_time.strftime("%Y-%m-%d")

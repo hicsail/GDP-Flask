9290a1843cf391b61e0db8dcfdbd86f2d0c0ded2
Greg Frasco <frascog17@gmail.com>
2025-03-14 17:46:01 -0400

    feat: run multiple classifiers

--- classifier/app.py
+++ classifier/app.py
@@ -213,7 +213,7 @@ def classify():
 if __name__ == '__main__':
     print(f"Updating {AI_SCORE}")
     load_dotenv()
-    scheduler.add_job(classify, "cron", hour="*", minute="*/5", max_instances=1)
+    scheduler.add_job(classify, "cron", hour="*", minute="*/5", max_instances=4)
     scheduler.start()
     #classify()
     print("Classifier schedule started")
